9351c393090071a723aa106e8b4fd36f01e16c6c
Nestornjrz <nestornjrz@hotmail.com>
2018-02-12 11:03:28 -0300

    Se crea el componente inicio

--- Ejemplo.Presentacion.Web/ngejemplo/src/app/app.module.ts
+++ Ejemplo.Presentacion.Web/ngejemplo/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
