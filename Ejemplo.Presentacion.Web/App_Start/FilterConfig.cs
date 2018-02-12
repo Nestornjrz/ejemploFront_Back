08d7a2970ec5bfce9c4a6ddd1d270b9c4f2ebac4
Nestornjrz <nestornjrz@hotmail.com>
2018-02-12 09:40:46 -0300

    Comienzo del proyecto MVC + Angular + SqlServer

---
+++ Ejemplo.Presentacion.Web/App_Start/FilterConfig.cs
@@ -0,0 +1,10 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo.Presentacion.Web {
+    public class FilterConfig {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
