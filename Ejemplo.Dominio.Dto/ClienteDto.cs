9eb15d1693455be5dab06bc013b439f9d1172e04
Nestornjrz <nestornjrz@hotmail.com>
2018-02-12 11:22:51 -0300

    Se agrego el DbContext y se creo el Dto

---
+++ Ejemplo.Dominio.Dto/ClienteDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Ejemplo.Dominio.Dto {
+    public class ClienteDto {
+        public int ClienteID { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public System.DateTime FechaNacimiento { get; set; }
+    }
+}
